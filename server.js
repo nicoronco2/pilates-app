cabc01885699182c627b7b5f677c7681307d9f9d
nico <409048@sistemas.frc.utn.edu.ar>
2026-04-02 01:01:20 -0300

    secure true

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: isProduction,
+        secure: true,
         sameSite: "lax",
         maxAge: 1000 * 60 * 60
     }

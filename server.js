1d9c96b24cf53ec42a2552c2c362620eee15e7a0
nico <409048@sistemas.frc.utn.edu.ar>
2026-04-02 02:16:40 -0300

    admin actuali

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: isProduction, // 🔥 SOLO en producción
-        sameSite: isProduction ? "none" : "lax",
+        sameSite: "lax",
         maxAge: 1000 * 60 * 60
     }
 }));
